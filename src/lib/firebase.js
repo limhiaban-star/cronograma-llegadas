04d6a09f9c69b19aa018fd8aad1ce710422ac7df
limhiaban-star <limhi.aban@macropay.mx>
2026-08-27 12:35:02 -0600

    Connect to Firebase for global real-time orders

---
+++ src/lib/firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBhIRZwKGapdgHxBSKBbc4xHqzx4xsVzRA",
+  authDomain: "cronograma-de-llegadas.firebaseapp.com",
+  projectId: "cronograma-de-llegadas",
+  storageBucket: "cronograma-de-llegadas.firebasestorage.app",
+  messagingSenderId: "809474428136",
+  appId: "1:809474428136:web:e7ff2a7d1077cacfb28d5d"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
