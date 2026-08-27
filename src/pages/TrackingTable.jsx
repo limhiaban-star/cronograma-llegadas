f6b77f2c0f0e993de6f07f45386456f285995913
limhiaban-star <limhi.aban@macropay.mx>
2026-08-27 10:41:06 -0600

    Add edit button to OrderDetailsModal to allow entering received amount/date

--- src/pages/TrackingTable.jsx
+++ src/pages/TrackingTable.jsx
@@ -297,7 +297,14 @@ export default function TrackingTable() {
       )}
       
       {viewingOrder && (
-        <OrderDetailsModal order={viewingOrder} onClose={() => setViewingOrder(null)} />
+        <OrderDetailsModal 
+          order={viewingOrder} 
+          onClose={() => setViewingOrder(null)} 
+          onEdit={(order) => {
+            setViewingOrder(null);
+            setEditingOrder(order);
+          }}
+        />
       )}
     </div>
   );
